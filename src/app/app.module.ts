dae0fb016f0db51b98a2cc699d0bf64676cc5472
Bas Pennings <baspster@gmail.com>
2020-10-04 12:44:17 +0200

    removed routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GeneratorsComponent } from './generators/generators.component';
-import { AppRoutingModule } from './app-routing.module';
 import { OptionsComponent } from './options/options.component';
 
 @NgModule({
@@ -14,7 +13,6 @@ import { OptionsComponent } from './options/options.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
