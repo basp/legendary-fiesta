4c74e3d53cf9624fd660d95585f97105f2deb694
Bas Pennings <baspster@gmail.com>
2020-10-08 19:37:53 +0200

    more quotes!

--- src/app/achievements/achievements.component.ts
+++ src/app/achievements/achievements.component.ts
@@ -1,4 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
+const QUOTES: Quote[] = [
+  {
+    text: 'Trees that are slow to grow bear the best fruit.',
+    author: 'Moliere',
+    title: null,
+  },
+  {
+    text: 'The strongest of all warriors are these two — Time and Patience.',
+    author: 'Leo Tolstoy',
+    title: 'War and Peace',
+  },
+  {
+    text: 'Make your ego porous. Will is of little importance, complaining is nothing, fame is nothing. Openness, patience, receptivity, solitude is everything.',
+    author: 'Rainer Maria Rilke',
+    title: null,
+  },
+  {
+    text: 'Everybody going to be dead one day, just give them time.',
+    author: 'Neil Gaiman',
+    title: 'Anansi Boys',
+  },
+  {
+    text: 'I am extraordinarily patient, provided I get my own way in the end.',
+    author: 'Margaret Thatcher',
+    title: null,
+  },
+];
+
+function getRandomQuote(): Quote {
+  var i = Math.floor(Math.random() * QUOTES.length);
+  return QUOTES[i];
+}
 
 const ACHIEVEMENTS = [];
 
@@ -8,10 +42,11 @@ const ACHIEVEMENTS = [];
   styleUrls: ['./achievements.component.css']
 })
 export class AchievementsComponent implements OnInit {
+  quote: Quote;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.quote = getRandomQuote();
   }
-
 }
